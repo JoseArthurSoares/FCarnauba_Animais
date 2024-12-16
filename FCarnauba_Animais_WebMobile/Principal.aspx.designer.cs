726dd38612161b00837451301bf10b0a03a326c1
JoseArthurSoares <arthursoares.js@gmail.com>
2024-12-16 15:44:49 -0300

    Alteração no design no módulo Leiteiro

--- FCarnauba_Animais_WebMobile/Principal.aspx.designer.cs
+++ FCarnauba_Animais_WebMobile/Principal.aspx.designer.cs
@@ -7,27 +7,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FCarnauba_Animais_WebMobile {
-    
-    
-    public partial class Principal {
-        
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// btnSair control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnSair;
+namespace FCarnauba_Animais_WebMobile
+{
+
+
+    public partial class Principal
+    {
     }
 }
